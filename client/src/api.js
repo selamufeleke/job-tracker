3b3273ffa36f56a1f595316769df09080fd7d5c4
Selamu <selamufeleke1@gmail.com>
2024-07-15 11:22:04 +0300

    Point frontend to live backend URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://job-tracker-api-46o1.onrender.com/api",
 });
 
 // Automatically attach the token to every request, if we have one saved
